5f0a5e9e078a41e131f2ad18318742a185aa52f1
aprlfch <aprlianfachrul@gmail.com>
2022-06-14 18:09:41 +0800

    Add search bar in Object Tourism, Culinary, Accomodation and Fix warning in Page About

--- src/pages/Culinary/index.js
+++ src/pages/Culinary/index.js
@@ -5,44 +5,42 @@ const Culinary = () => {
   return (
     <div className='main'>
       <div className='hero_text'>
-        <h1 className="hero_title">Rasakan Wisata Kuliner Indonesia</h1>
-        <p className="hero_tagline">Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit
+        <h1 className='hero_title'>Rasakan Wisata Kuliner Indonesia</h1>
+        <p className='hero_tagline'>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit
           officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.</p>
       </div>
 
-      <div class="barContainer">
-        {/* <div class="search-box">
-          <input class="input" type="text" placeholder="Search..." />
+      <div className='barContainerCulinary'>
+        <div className='search-box-culinary'>
+          <input className='inputCulinary' typeof='text' placeholder='Search' />
         </div>
-
-        <div class="addBox">
-          <a href="##" id="icon">
-            <i class="fa-solid fa-plus"></i>
-          </a>
-        </div> */}
       </div>
 
-      <div class="containerCardCulinary">
-        <div class="box">
-          <div class="textCulinary">
+      <div className='containerCardCulinary'>
+
+        <div className='box'>
+          <div className='textCulinary'>
             <h1>Bakso</h1>
             <p>Solo</p>
           </div>
         </div>
-        <div class="box two-Culinary">
-          <div class="textCulinary">
+
+        <div className='box two-Culinary'>
+          <div className='textCulinary'>
             <h1>Kwetiau Goreng</h1>
             <p>Malang</p>
           </div>
         </div>
-        <div class="box three-Culinary">
-          <div class="textCulinary">
+
+        <div className='box three-Culinary'>
+          <div className=' textCulinary'>
             <h1>Nasi Goreng </h1>
             <p>Magelang</p>
           </div>
         </div>
+
       </div>
-    </div>
+    </div >
   )
 }
 
--- src/pages/ObjectTourism/index.js
+++ src/pages/ObjectTourism/index.js
@@ -5,49 +5,41 @@ import './styles/objectTourism.css'
 const ObjectTourism = () => {
   return (
     <div className='main'>
-      <div class="hero_text">
-        <h1 class="hero_title">Objek Wisata Terbaik di Indonesia</h1>
-        <p class="hero_tagline">Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit
+      <div className='hero_text'>
+        <h1 className='hero_title'>Objek Wisata Terbaik di Indonesia</h1>
+        <p className='hero_tagline'>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit
           officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.</p>
       </div>
-      <div class="barContainer">
-        {/* <div class="search-box">
-          <input class="input" type="text" placeholder="Search..." />
-          <a href="##" class="icon">
-            <i class="fa-solid fa-magnifying-glass"></i>
-          </a>
-        </div>
 
-        <div class="addBox">
-          <a href="##" id="icon">
-            <i class="fa-solid fa-plus"></i>
-          </a>
-        </div> */}
+      <div className='barContaineroBbjectTourism'>
+        <div className='search-box-objectTourism'>
+          <input className='inputobjectTourism' typeof='text' placeholder='Search' />
+        </div>
       </div>
 
-      <div class="containerCardObjectTourism">
-        <div class="boxs">
-          <div class="text-objectTourism">
+      <div className='containerCardObjectTourism'>
+        <div className='boxs'>
+          <div className='text-objectTourism'>
             <h1>Pantai Kuta</h1>
             <p>Bali</p>
           </div>
         </div>
-        <div class="boxs two-objectTourism">
-          <div class="text-objectTourism">
+
+        <div className='boxs two-objectTourism'>
+          <div className='text-objectTourism'>
             <h1>Candi Borobudur </h1>
             <p>Magelang</p>
-
           </div>
         </div>
 
-        <div class="boxs three-objectTourism">
-          <div class="text-objectTourism">
+        <div className='boxs three-objectTourism'>
+          <div className='text-objectTourism'>
             <h1>Gunung Semeru</h1>
             <p>Malang</p>
           </div>
         </div>
       </div>
-    </div>
+    </div >
   )
 }
 
