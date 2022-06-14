40b95c2a5dcfb0c079d3b337b20cb827a6ce3000
aprlfch <aprlianfachrul@gmail.com>
2022-06-15 00:42:28 +0800

    Create Add Object Tourism, Add Culinary, Add Accomodation Pages

---
+++ src/pages/AddAccomodation/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './addAccomodation.css'
+
+const AddAccomodation = () => {
+    return (
+        <div className="main">
+            <p>addAccomodation Pages</p>
+        </div>
+    )
+};
+
+export default AddAccomodation;
---
+++ src/pages/addObjectTourism/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './addObjectTourism.css'
+
+const AddObjectTourism = () => {
+    return (
+        <div className="main">
+            <p>Add Object Tourism</p>
+        </div>
+    )
+};
+
+export default AddObjectTourism;
