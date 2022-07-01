2e4a814a4f37d15ffc246e3768a1d2f68c1dd28c
kadekframa <dframa19@gmail.com>
2022-07-01 22:50:50 +0800

    Updated import images in assets index.js

--- src/assets/index.js
+++ src/assets/index.js
@@ -2,11 +2,19 @@ import Logo from './images/mlaku-logo-sm.png';
 import objecttourismIllustration from "./images/object-tourism.png";
 import culinaryIllustration from "./images/culinary.png";
 import accommodationIllustration from "./images/accommodation.png";
+import Dev1 from './images/dev-1.jpeg';
+import Dev2 from './images/dev-2.jpeg';
+import Dev3 from './images/dev-3.jpeg';
+import Dev4 from './images/dev-4.jpg';
 
 
 export {
     objecttourismIllustration,
     culinaryIllustration,
     accommodationIllustration,
-    Logo
+    Logo,
+    Dev1,
+    Dev2,
+    Dev3,
+    Dev4,
 }
