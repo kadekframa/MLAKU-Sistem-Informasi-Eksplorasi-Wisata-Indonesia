fbec4cf2d6bc11daf38e8eb07a3ae00b4af9a1ab
kadekframa <dframa19@gmail.com>
2022-07-02 16:31:30 +0800

    Fix Conflict ini culinary card and acomdation index

--- src/components/CulinaryCard/index.js
+++ src/components/CulinaryCard/index.js
@@ -2,15 +2,15 @@ import React from 'react';
 import './culinary-card.css';
 
 const CulinaryCard = (props) => {
-  const { culinary_name, address, image, _id } = props;
+  const { culinary_name, address, image, id } = props;
   return (
     <div className='component-wrapper-culinary'>
       <div className='img-wrapper-culinary'>
-        <a href={`/detail-culinary/${_id}`}>
+        <a href={`/detail-culinary/${id}`}>
           <img src={image} alt={culinary_name} title={`Gambar Suasana Objek Wisata ${culinary_name}`} />
         </a>
         <div className='text-wrapper-culinary'>
-          <a href={`/detail-culinary/${_id}`}>
+          <a href={`/detail-culinary/${id}`}>
             <p className='culinary-name'>{culinary_name}</p>
           </a>
           <p className='culinary-addres'>{address}</p>
